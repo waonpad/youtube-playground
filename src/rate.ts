d89fbf769ed1d4687bde17be46503d5d059bc1f6
waonpad <waonpad@gmail.com>
2025-04-06 01:36:17 +0900

    れ

--- src/rate.ts
+++ src/rate.ts
@@ -1,4 +1,5 @@
 import type { youtube_v3 } from "googleapis";
+import { Common } from "googleapis";
 
 /**
  * @see [Videos: rate  |  YouTube Data API  |  Google for Developers](https://developers.google.com/youtube/v3/docs/videos/rate?hl=ja)
@@ -26,8 +27,42 @@ export const rateAllVideos = async (
     // biome-ignore lint/style/noNonNullAssertion: <explanation>
     .map((rate) => rate.videoId!);
 
-  for (const id of notRatedVideoIds) {
-    await youtube.videos.rate({ id, rating });
+  const ratinsDisabledVideoIds: string[] = [];
+
+  try {
+    for (const id of notRatedVideoIds) {
+      try {
+        await youtube.videos.rate({ id: "あ", rating });
+      } catch (e) {
+        if (e instanceof Common.GaxiosError) {
+          // 型をつける
+          const gaxiosError = e as Common.GaxiosError;
+
+          /**
+           * 以下リンクのError detailの部分
+           *
+           * https://developers.google.com/youtube/v3/docs/videos/rate#errors
+           */
+          const isvideoRatingDisabled = gaxiosError.response?.data.error.errors.some(
+            ({ reason }) => reason === "videoRatingDisabled",
+          );
+
+          // 評価が無効な動画の場合、リストに追加してそのまま継続
+          if (isvideoRatingDisabled) {
+            ratinsDisabledVideoIds.push(id);
+            continue;
+          }
+        }
+
+        // 対応不可能なエラーは再スローする
+        throw e;
+      }
+    }
+  } finally {
+    if (ratinsDisabledVideoIds.length) {
+      // TODO: どこかに通知
+      console.log("評価が無効な動画ID: ", ratinsDisabledVideoIds);
+    }
   }
 
   return {
